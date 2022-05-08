47dfe0e0a244bdd91dda9137b93bcf2a1e706f2f
LizAntoinette <lizanguren12@gmail.com>
2022-05-08 22:52:34 +0800

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
+import particlesJS from "./scripts/particles";
+import Typed from './scripts/typed.js';
+
+
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
+
 initScrollReveal(targetElements, defaultProps);
+
 initTiltEffect();
+
---
+++ src/scripts/typed.js
@@ -0,0 +1,15 @@
+var options = {
+  strings: [
+      "I'm a <span class=\"pine-text\">Software Engineer</span>", 
+      "I build things for the <span class=\"pine-text\">Web</span>",
+      "I do <span class=\"pine-text\">Wizardry</span> with Codes"
+  ],
+  typeSpeed: 60,
+  startDelay: 50,
+  backSpeed: 70,
+  backDelay: 1500,
+  loop:true
+
+};
+
+var typed = new Typed('.work-title', options);
