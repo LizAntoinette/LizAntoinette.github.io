c7bbbabb6b5603064d620682b185d96f7459438b
LizAntoinette <lizanguren12@gmail.com>
2022-11-16 01:25:26 +0800

    Update for other sections

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import particlesJS from "./scripts/particles";
 import Typed from './scripts/typed.js';
+import Global from './scripts/global.js';
 
 
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
