a6a9732baac3944529ab66d936f43a3c60b4a304
LizAntoinette <lizanguren12@gmail.com>
2022-11-17 01:29:02 +0800

    Updates

---
+++ scripts/global.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+
+    
+    $('.nav-item').click(function() {
+      $(this).siblings().removeClass('active');
+      $(this).addClass('active');
+    });
+
+    
+});
+
+
+// Cache selectors
+var topMenu = $(".nav"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+
+  //Nav bar hide on scroll
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-7.5rem";
+  }
+  prevScrollpos = currentScrollPos;
+
+
+  var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   // Set/remove active class
+   menuItems
+     .parent().removeClass("active")
+     .end().filter("[href='#"+id+"']").parent().addClass("active");
+
+
+
+}
